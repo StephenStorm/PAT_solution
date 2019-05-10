1b7cd5973171508823aa647aac9bb51c83657ecc
stephen <1561610536@qq.com>
2019-05-10 23:13:58 +0800

    Add files via upload

--- AreTheyEqual.cpp
+++ AreTheyEqual.cpp
@@ -6,9 +6,9 @@ using namespace std;
 string transform(string str,int n){
 	string res="0.";
 	int a = str.find_first_of('.'),b=0;
-	a=a>0?a:str.size();//?????? 
+	a=a>0?a:str.size();//小数点的位置 
 	while(b<str.length()&&(str[b]==48|| str[b]=='.') )b++;
-	//??????????
+	//第一个有效数字的位置
 	if(b==str.size()) {
 		for(int i=0;i<n;i++)
 			res.append(1,'0');
@@ -18,8 +18,10 @@ string transform(string str,int n){
 	int power=a-b,count=0;
 	if(power<0) power++;
 	for(int i=b;count<n&&i<str.length();i++){
-		if(str[b]!='.') res.append(1,str[i]);
-		count++;
+		if(str[i]!='.'){
+            res.append(1,str[i]);
+		    count++;
+        } 
 	}
 	while(count<n){
 		res.append(1,'0');
@@ -29,17 +31,14 @@ string transform(string str,int n){
 	return res;
 }
 int main(){
-	int i,n=3;
+	int i,n;
 	string str1,str2,t1,t2;
-//	cin>>n>>str1>>str2;
-while(1){
-	cin>>str1>>str2;
+	cin>>n>>str1>>str2;
 	t1=transform(str1,n);
 	t2=transform(str2,n);
 	if(t1==t2){
 		cout<<"YES "<<t1; 
 	} 
 	else cout<<"NO "<<t1<<" "<<t2<<endl;
-}
 	return 0;
 }
